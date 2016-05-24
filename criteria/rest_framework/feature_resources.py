c4f5620ba7d4d7f84b9e2554d188f6128a24aba4
Premanand Achuthan <prem.apa@gmail.com>
2016-05-24 13:28:29 +0100

    added DRF

--- criteria/rest_framework/feature_resources.py
+++ criteria/rest_framework/feature_resources.py
@@ -1,12 +1,5 @@
 ''' Define a resource for criteria data to be used in Django REST framework. '''
 from django.http.response import Http404
-from rest_framework.filters import DjangoFilterBackend, OrderingFilter
-from rest_framework.response import Response
-from criteria.helper.gene_criteria import GeneCriteria
-from criteria.helper.marker_criteria import MarkerCriteria
-from criteria.helper.region_criteria import RegionCriteria
-from criteria.helper.study_criteria import StudyCriteria
-import re
 from criteria.helper.criteria import Criteria
 from elastic.rest_framework.resources import ListElasticMixin,\
     ElasticFilterBackend, RetrieveElasticMixin
@@ -14,66 +7,6 @@ from elastic.search import ElasticQuery, Search
 from elastic.query import Query
 from elastic.rest_framework.elastic_obj import ElasticObject
 from elastic.elastic_settings import ElasticSettings
-
-
-# class CriteriaFilterBackend(OrderingFilter, DjangoFilterBackend):
-# 
-#     FEATURE_TYPE_LIST = ['GENE', 'MARKER', 'REGION', 'STUDY', 'ALL']
-# 
-#     def filter_queryset(self, request, queryset, view):
-#         ''' Override this method to request feature locations. '''
-#         try:
-#             filterable = getattr(view, 'filter_fields', [])
-#             filters = dict([(k, v) for k, v in request.GET.items() if k in filterable])
-#             feature_type_str = filters.get('feature_type', 'GENE')
-#             feature_id = filters.get('feature_id')
-# 
-#             identifiers_list = []
-#             identifiers_list = re.split('\n|,', feature_id)
-#             identifiers = [identifier.rstrip() for identifier in identifiers_list]
-# 
-#             print('=================')
-#             print(feature_type_str)
-#             print(identifiers)
-#             print(request.data)
-#             print('=================')
-# 
-#             criteria_details = None
-#             if feature_type_str == 'GENE':
-#                 criteria_details = GeneCriteria.get_all_criteria_disease_tags(identifiers)
-#             elif feature_type_str == 'MARKER':
-#                 criteria_details = MarkerCriteria.get_all_criteria_disease_tags(identifiers)
-#             elif feature_type_str == 'REGION':
-#                 criteria_details = RegionCriteria.get_all_criteria_disease_tags(identifiers)
-#             elif feature_type_str == 'STUDY':
-#                 criteria_details = StudyCriteria.get_all_criteria_disease_tags(identifiers)
-#             elif feature_type_str == 'ALL':
-#                 criteria_details = Criteria.do_criteria_search(identifiers)
-# 
-#             criterias = []
-#             disease_tags = []
-#             if feature_type_str == 'ALL':
-#                 for feature_type, feature_result in criteria_details.items():
-#                     for feature_id, feature_value in feature_result.items():
-#                         if 'all' in feature_value:
-#                             disease_tags = feature_value['all']
-#                             criterias.append({'feature_type': feature_type.upper(),
-#                                               'feature_id': feature_id,
-#                                               'disease_tags': disease_tags,
-#                                               'feature_name': feature_id})
-#             else:
-#                 for feature_id, feature_value in criteria_details.items():
-#                     print(feature_id)
-#                     if 'all' in feature_value:
-#                         disease_tags = feature_value['all']
-#                     criterias.append({'feature_type': feature_type_str,
-#                                       'feature_id': feature_id,
-#                                       'disease_tags': disease_tags,
-#                                       'feature_name': feature_id})
-# 
-#             return criterias
-#         except (TypeError, ValueError, IndexError, ConnectionError):
-#             raise Http404
 
 
 class CriteriaFilterBackend(ElasticFilterBackend):
@@ -100,10 +33,13 @@ class CriteriaFilterBackend(ElasticFilterBackend):
         q_from = view.paginator.get_offset(request)
 
         filterable = getattr(view, 'filter_fields', [])
-        print(filterable)
-        print(request)
         filters = dict([(k, v) for k, v in request.GET.items() if k in filterable])
         criteria_idx = self._get_index(filters.get('feature_type', 'GENE_CRITERIA'))
+
+        feature_type = filters.get('feature_type')
+        feature_id = filters.get('feature_id')
+        aggregate = filters.get('aggregate')
+        detail = filters.get('detail')
 
         idx = criteria_idx
         if type(criteria_idx) == list:
@@ -111,17 +47,130 @@ class CriteriaFilterBackend(ElasticFilterBackend):
         else:
             idx = ElasticSettings.idx(criteria_idx)
 
-        q = ElasticQuery(Query.match_all())
-        s = Search(search_query=q, idx=idx, size=q_size, search_from=q_from)
-        json_results = s.get_json_response()
         results = []
-        for result in json_results['hits']['hits']:
-            new_obj = ElasticObject(initial=result['_source'])
-            new_obj.uuid = result['_id']
-            new_obj.criteria_type = result['_type']
+        if feature_id and aggregate == 'true':
+            disease_doc_tags = Criteria.get_disease_tags(feature_id, idx=idx)
+            disease_tags = [getattr(d, 'code') for d in disease_doc_tags]
+            new_obj = ElasticObject()
+            new_obj.disease_tags = disease_tags
+            new_obj.criteria_type = None
             results.append(new_obj)
-        view.es_count = json_results['hits']['total']
-        return results
+            return results
+        elif feature_id and detail == 'true':
+            (idx, idx_types) = Criteria.get_feature_idx_n_idxtypes(feature_type.lower())
+            criteria_details = Criteria.get_criteria_details(feature_id, idx=idx, idx_type=idx_types)
+            criteria_list = idx_types.split(',')
+            criteria_details_expanded = Criteria.add_meta_info(idx, criteria_list, criteria_details)
+
+            feature_details = self._get_feature_details(criteria_details_expanded)
+
+            for criteria, details in feature_details.items():
+                print(criteria)
+                new_obj = ElasticObject()
+                new_obj.qid = details['qid']
+                new_obj.criteria_type = criteria
+                disease_tags = details['disease_tags']
+                fdetails = list(details['fdetails'])
+                print('+++++++++++++++')
+                print(disease_tags)
+                print(fdetails)
+                print('+++++++++++++++')
+                new_obj.disease_tags = disease_tags
+                new_obj.feature_details = fdetails
+                results.append(new_obj)
+
+            return results
+        else:
+            q = ElasticQuery(Query.match_all())
+            s = Search(search_query=q, idx=idx, size=q_size, search_from=q_from)
+            json_results = s.get_json_response()
+
+            for result in json_results['hits']['hits']:
+                new_obj = ElasticObject(initial=result['_source'])
+                new_obj.uuid = result['_id']
+                new_obj.criteria_type = result['_type']
+                results.append(new_obj)
+
+            view.es_count = json_results['hits']['total']
+            return results
+
+    def _get_feature_details(self, criteria_details_expanded):
+
+        feature_details = {}
+        hits = criteria_details_expanded['hits']
+        meta_info = criteria_details_expanded['meta_info']
+        link_info = criteria_details_expanded['link_info']
+
+        for hit in hits:
+            _source = hit['_source']
+            _index = hit['_index']
+            _type = hit['_type']
+            _id = hit['_id']
+            _disease_tags = _source['disease_tags']
+            _qid = _source['qid']
+            link_id_type = link_info[_index][_type]
+            _type_desc = meta_info[_index][_type]
+
+            for dis in _disease_tags:
+                fdetails = _source[dis]
+                for fdetail in fdetails:
+                    fid = fdetail['fid']
+                    fname = fdetail['fname']
+
+                    current_link = ""
+                    current_row = ""
+                    current_row += '<a href="/' + link_id_type + '/' + fid + '/">'
+                    current_row += fname
+                    current_row += '</a>'
+
+                    print(current_row)
+
+                    if 'fnotes' in fdetail:
+                        fnotes = fdetail['fnotes']
+                        print(fnotes)
+                        link_data = ''
+                        link_value = ''
+                        link_id = ''
+                        link_name = ''
+
+                        if 'linkdata' in fnotes:
+                            link_data = fnotes['linkdata']
+                        if 'linkvalue' in fnotes:
+                            link_value = fnotes['linkvalue']
+                        if 'linkid' in fnotes:
+                            link_id = fnotes['linkid']
+                        if 'linkname' in fnotes:
+                            link_name = fnotes['linkname']
+
+                        if link_data and link_value:
+                            current_row += ' ('+link_data+':'+str(link_value) + ')'
+
+                        if link_id and link_name:
+                            current_link += '<a href="/' + 'study' + '/' + link_id + '/">'
+                            current_link += link_name
+                            current_link += '</a>'
+
+                if(len(current_link) > 0):
+                    current_row += " - " + current_link
+
+                print(current_row)
+
+                if _type_desc in feature_details:
+                    tmp_list = feature_details[_type_desc]['fdetails']
+                    tmp_list.add(current_row)
+                    feature_details[_type_desc]['fdetails'] = tmp_list
+                    feature_details[_type_desc]['disease_tags'] = _disease_tags
+                else:
+                    tmp_list = set()
+                    feature_details[_type_desc] = {}
+                    tmp_list.add(current_row)
+                    feature_details[_type_desc]['fdetails'] = tmp_list
+                    feature_details[_type_desc]['disease_tags'] = _disease_tags
+
+                # Add the qid
+                feature_details[_type_desc]['qid'] = _qid
+
+        return feature_details
 
 
 class ListCriteriaMixin(ListElasticMixin):
--- criteria/rest_framework/rest_api.py
+++ criteria/rest_framework/rest_api.py
@@ -10,6 +10,7 @@ class CriteriaSerializer(serializers.Serializer):
     qid = serializers.CharField(help_text='feature id', required=True)
     criteria_type = serializers.CharField(help_text='index_type', required=True)
     disease_tags = serializers.ListField(help_text='disease tags')
+    feature_details = serializers.ListField(help_text='feature_details')
 
 
 class CriteriaViewSet(ListCriteriaMixin, mixins.ListModelMixin, GenericViewSet):
@@ -25,8 +26,25 @@ class CriteriaViewSet(ListCriteriaMixin, mixins.ListModelMixin, GenericViewSet):
               defaultValue: 'GENE'
               enum: ['GENE', 'MARKER', 'STUDY', 'REGION', 'ALL']
               paramType: query
+            - name: feature_id
+              description: Feature id (e.g. ENSG00000134242,rs2476601)
+              required: false
+              type: string
+              paramType: query
+            - name: aggregate
+              defaultValue: false
+              enum: [true, false]
+              description: aggregate disease_tags across all index types for a given feature_id
+              type: boolean
+              paramType: query
+            - name: detail
+              defaultValue: false
+              enum: [true, false]
+              description: feature details
+              type: boolean
+              paramType: query
     '''
 
     serializer_class = CriteriaSerializer
     pagination_class = ElasticLimitOffsetPagination
-    filter_fields = ('feature_type')
+    filter_fields = ('feature_type', 'feature_id', 'aggregate', 'detail')
